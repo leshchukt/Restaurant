eb5a1ba36f94dc764bb13ac88e0159a8e810de2e
Tetiana Leshchuk <leshchuk.tetiana@gmail.com>
2018-01-15 12:51:21 +0200

    added properties

---
+++ src/main/java/model/dao/ConnectionDao.java
@@ -0,0 +1,12 @@
+package model.dao;
+
+public interface ConnectionDao extends AutoCloseable {
+
+    void beginTransaction();
+
+    void commitTransaction();
+
+    void rollbackTransaction();
+
+    void close();
+}
